a15994a3e09639096a69089c9c94b3fe47b01da2
seventinylantemona17 <lseventiny@gmail.com>
2023-03-30 13:03:28 +0700

    pertemuan6

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,21 +2,12 @@
 function card() {
     return (
         <>
-         <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-blue-500 to-blue-600">
+         <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-blue-500 to-purple-600">
             <div class="w-1/2 h-4/2 bg-white rounded-lg shadow-2xl p-8 m-4">
                 <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">
-                    KTP
+                    Register
                 </h1>
                 <form action="/" method="post">
-                    <div class="flex flex-col mb-4">
-                        <label
-                         class="mb-2 font-bold text-lg text-gray-900"
-                         for="tempat_lahir"
-                        >
-                            Foto
-                        </label>
-                        <img src={process.env.PUBLIC_URL + '/seventiny.jpg'} class="flex flex-col mb-1 w-40 h-60" />
-                    </div>
                     
                     <div class="flex flex-col mb-4">
                         <label
@@ -27,7 +18,7 @@ function card() {
                         </label>
                         <input
                          class="border py-2 px-3 text-grey-800"
-                         type="text"placeholder="Seventiny Lantemona"
+                         type="text" placeholder="Seventiny Lantemona"
                          name="nama"
                          id="nama"
                         />
@@ -35,32 +26,71 @@ function card() {
                     <div class="flex flex-col mb-4">
                         <label
                          class="mb-2 font-bold text-lg text-gray-900"
-                         for="alamat"
+                         for="gender"
                         >
-                            Alamat
+                            Gender
                         </label>
+                        <div>
                         <input
-                         class="border py-2 px-3 text-lg text-gray-900"
-                         type="text"placeholder="Tawoali"
-                         name="alamat"
-                         id="alamat"
+                         class="border py-2 text-grey-800"
+                         type="radio"
+                         name="gender"
+                         id="pria"
                         />
+                       
+                        <label 
+                            class="mb-2 text-gray-900" 
+                            for="pria"
+                        >
+                            Pria
+                        </label>
+                        </div>
+                    </div>
+                    <div>
+                        <input
+                            class="border py-2 text-grey-800"
+                            type="radio"
+                            name="gender"
+                            id="wanita"
+                        />
+                        <label
+                        class="mb-2 text-gray-900"
+                        for="wanita"
+                        >Wanita</label>
                     </div>
                     <div class="flex flex-col mb-4">
                         <label
                          class="mb-2 font-bold text-lg text-gray-900"
-                         for="jenis_kelamin"
+                         for="agama"
                         >
-                            Jenis Kelamin
+                           Agama
                         </label>
-                        <input
-                         class="border py-2 px-3 text-lg text-gray-900"
-                         type="text"placeholder="Perempuan"
-                         name="jenis_kelamin"
-                         id="jenis_kelamin"
-                        />
+                        <select
+                            class="border py-2 px-3 text-grey-800"
+                            name="agama"
+                            id="agama"
+                        >
+                            <option value="Protestan">Kristen Protestan</option>
+                            <option value="Katholik">Katholik</option>
+                            <option value="Islam">Islam</option>
+                            <option value="Hindu">Hindu</option>
+                            <option value="Budha">Budha</option>
+                            <option value="Kepercayaan">Aliran Kepercayaan</option>
+                            </select>
                     </div>
+                    <button
+                    class="block bg-blue-600 hover:bg-blue-00 text-white uppercase texty-lg mx-auto p-4 rounded"
+                    type="submit"
+                    >
+                        Buat Akun
+                    </button>
                 </form>
+                <a
+                    class="block w-full texty-center no-underline mt-4 text-sm text-gray-700 hover:texty-gray-900"
+                    href="/"
+                >
+                    Telah Memiliki Akun?
+                </a>
             </div>
          </div>
         </>
